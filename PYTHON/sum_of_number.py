b22f08a6a1e2bbd4fbd52e46cf715e460e6129b0
RIPON-SINGHA <riponsingha722@gmail.com>
2026-01-13 13:15:23 +0530

    sun of numbers using loop and func

---
+++ PYTHON/sum_of_number.py
@@ -0,0 +1,17 @@
+def sum_upto(n):
+    ans = 0
+    for i in range(1, n+1):
+        ans = ans + i
+    return ans
+
+n = int(input("What will be the range? "))
+result = sum_upto(n)
+print(result)
+
+
+
+# ans = 0
+# for i in range (1,6):
+#     ans = ans + i
+
+# print(ans)
