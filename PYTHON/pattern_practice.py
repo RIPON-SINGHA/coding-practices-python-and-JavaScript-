20c018c6ef89d0276cd9fa4446e48c236fe41bf3
RIPON-SINGHA <riponsingha722@gmail.com>
2026-01-13 14:58:44 +0530

    Practicing patterns using nested loops

---
+++ PYTHON/pattern_practice.py
@@ -0,0 +1,79 @@
+# LEFT ANGLED INVERTED TRIANGLE
+for i in range(5, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+for i in range(5, 0, -1):
+    print("*" * i, sep="")
+    
+    
+    
+    
+    
+    
+    
+    
+# RIGHT ANGLED TRIANGLE    
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+for i in range(1, 6):
+    print("*" * i, sep="")
+
+
+
+
+
+
+
+# LEFT ANGLED TRIANGLE
+for i in range(1, 5+1):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(i):
+        print("*", end="")
+    print()
+
+
+
+
+
+
+# THESE BOTH ARE FOR RIGHT ANGLED INVERTED TRIANGLE
+for i in range(1, 6):
+    for j in range(6, i, -1):
+        print("*", end="")
+    print()
+    for k in range(i):
+        print(" ", end="")
+        
+        
+
+        
+for i in range(1, 6):
+    for j in range(i - 1):
+        print(" ", end="")
+    for k in range(6 - i):
+        print("*", end="")
+    print()
+        
+        
+        
+        
+        
+        
+        
+# SQUARE        
+for i in range(1,6):
+    for j in range(5):
+        print("*", end="")
+    print()
+        
+
+
+# MORE PATTERNS TO COME LATER
