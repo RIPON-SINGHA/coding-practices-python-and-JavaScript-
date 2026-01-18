9b68e19e209ebd0d5fe558eb5e6489e05b72a074
RIPON-SINGHA <riponsingha722@gmail.com>
2026-01-18 11:46:16 +0530

    sun of digit problem

--- PYTHON/sum_of_digit.py
+++ PYTHON/sum_of_digit.py
@@ -1,5 +1,5 @@
 def main():
-    number = float(input("Enter your number: "))
+    number = int(input("Enter your number: "))
     result = sumDigit(number)
     print(f"The answer of sum of your digit is {result}")
 
@@ -7,16 +7,15 @@ def main():
 def sumDigit(n): # 1234
     if n == 0:
         return 0
-    rev = 0
-    sum = 0
+    digit_sum = 0
     n = abs(n)
 
     while n > 0:
         temp = n % 10 # 4, 3, 2, 1
-        sum = sum + temp # 4, 7, 9, 10
+        digit_sum = digit_sum + temp # 4, 7, 9, 10
         n = n // 10 # 123, 12, 1, 0 
 
-    return sum
+    return digit_sum
 
 
 main()
